de4ec1b7ed0ab79441f61de8683f67e6a94a1838
jackbaber <thejackbabs@gmail.com>
2018-03-19 10:37:23 +0000

    enabled navigation from newpost page to index page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
 
 class PostsNew extends Component {
   renderField(field){
@@ -19,9 +20,10 @@ class PostsNew extends Component {
       </div>
     )
   }
-  // onSubmit(values){
-  //   this.props.createPost
-  // }
+
+  onSubmit(values){
+    console.log(values)
+  }
   render(){
     const { handleSubmit } = this.props
     return (
@@ -42,6 +44,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
