5c5bdefa044bbd1aad33df163b8e0e27c02d9503
jackbaber <thejackbabs@gmail.com>
2018-03-19 10:59:57 +0000

    created navigation back to index on form submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createPost } from '../actions'
 
 class PostsNew extends Component {
   renderField(field){
@@ -22,7 +24,10 @@ class PostsNew extends Component {
   }
 
   onSubmit(values){
-    console.log(values)
+    this.props.createPost(values, () => {
+      this.props.history.push('/')
+    })
+
   }
   render(){
     const { handleSubmit } = this.props
@@ -67,4 +72,6 @@ function validate(values){
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew)
+})(
+  connect(null, { createPost })(PostsNew)
+)
