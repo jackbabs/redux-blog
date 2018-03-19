2cc66d9db413f8613dbbee7e4623d6cc6e7f0000
jackbaber <thejackbabs@gmail.com>
2018-03-19 11:45:11 +0000

    implemented check for non-existant post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import fetchPost from '../actions'
+import { fetchPost } from '../actions'
 
-class PostsShows extends Component {
+class PostsShow extends Component {
   componentDidMount(){
     const { id } = this.props.match.params
     this.props.fetchPost(id)
   }
 
   render(){
+    const { post } = this.props
+    if(!post){
+      return <div>Loading...</div>
+    }
+  
     return (
       <div>
-      PostsShow
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
